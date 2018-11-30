f95475411fa7c76374548b38aeeab69461fad528
Chris brown <elchrisso@hotmail.com>
2018-11-30 12:49:19 -0700

    Added magenta.js, midi data for twinkletwinkle and a play button, and verified the song plays locally.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
 import "./App.css";
+import * as mm from "@magenta/music";
 
 class App extends Component {
   render() {
+    const model = new mm.MusicVAE(
+      "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_lokl_small"
+    );
+
+    const TWINKLE_TWINKLE = {
+      notes: [
+        { pitch: 60, startTime: 0.0, endTime: 0.5 },
+        { pitch: 60, startTime: 0.5, endTime: 1.0 },
+        { pitch: 67, startTime: 1.0, endTime: 1.5 },
+        { pitch: 67, startTime: 1.5, endTime: 2.0 },
+        { pitch: 69, startTime: 2.0, endTime: 2.5 },
+        { pitch: 69, startTime: 2.5, endTime: 3.0 },
+        { pitch: 67, startTime: 3.0, endTime: 4.0 },
+        { pitch: 65, startTime: 4.0, endTime: 4.5 },
+        { pitch: 65, startTime: 4.5, endTime: 5.0 },
+        { pitch: 64, startTime: 5.0, endTime: 5.5 },
+        { pitch: 64, startTime: 5.5, endTime: 6.0 },
+        { pitch: 62, startTime: 6.0, endTime: 6.5 },
+        { pitch: 62, startTime: 6.5, endTime: 7.0 },
+        { pitch: 60, startTime: 7.0, endTime: 8.0 }
+      ],
+      totalTime: 8
+    };
+
+    const player = new mm.SoundFontPlayer(
+      "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
+    );
+
+    function play() {
+      player.start(TWINKLE_TWINKLE);
+    }
+
     return (
       <div className="App">
         <h1>this is elChrisso.</h1>
+        <button onClick={play}>Play</button>
       </div>
     );
   }
